decd3ed39057a371c4d513a2c3eec819bef3330e
myersm0 <michael.myers@wustl.edu>
2023-12-27 15:56:52 -0500

    Added exports

--- src/WordFrequencyDistributions.jl
+++ src/WordFrequencyDistributions.jl
@@ -9,10 +9,19 @@ using Chain
 abstract type CheckStyle end
 struct Safe <: CheckStyle end
 struct Fast <: CheckStyle end
+export CheckStyle, Safe, Fast
 
 include("corpora.jl")
+export Corpus, getindex, sample, occursin
+
 include("zipf.jl")
+export ZipfRank
+
 include("empirical.jl")
+export ω, V, N, f, p, g
+
 include("estimators.jl")
+export GaleSampson, NaranBalasub, Yule, Simpson, Guiraud, Sichel, Honore, Herdan
+export C
 
 end
