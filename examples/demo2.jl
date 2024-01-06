b9d6790d27d77a9a0b2e4e7d9b0235f5638b2572
myersm0 <michael.myers@wustl.edu>
2024-01-06 13:36:55 -0500

    Extended demo2

--- examples/demo2.jl
+++ examples/demo2.jl
@@ -3,17 +3,16 @@ using WordFrequencyDistributions
 using Chain
 using StatsBase: quantile
 using Pkg.Artifacts
+using GLMakie
+using Colors
 
 # This set of examples uses a small dataset I made for a topic modeling problem,
 # consisting of GPT-4-generated readme docs for 70 hypothetical projects/repos 
 # in 7 different categories: web design, space exploration, cooking, etc.
 
-# The methods here are based on those outlined in Baayen 2001 chapter 5, but with
-# some modification to handle this particular dataset
-
+# first set up the data:
 rootpath = artifact"medadata"
 filelist = [joinpath(rootpath, "$i.readme.md") for i in 1:70]
-classes = vcat([repeat([i], 10) for i in 1:7]...)
 
 tokenize(str::String) = @chain split(str, r"[^\w]+") filter(x -> x != "", _)
 
@@ -28,6 +27,11 @@ function process_file(filename)
 end
 
 docs = process_file.(filelist)
+
+
+# ===== Part 1: Vocabulary dispersion ==========================================
+# The methods here are based on those outlined in Baayen 2001 chapter 5, 
+# but with some modification to handle this particular dataset
 
 # make a single Corpus from the concatenated documents:
 c = Corpus(vcat(docs...))
@@ -72,5 +76,57 @@ result = map(
 thresh = quantile(result, 0.2)
 inds = findall(result .== 0)
 ω(c)[inds]
+
+
+# ===== Part 2: Vocabulary growth within a particular topic ====================
+
+# number the topics 1 through 7; they're in order, with 10 docs per topic
+classes = vcat([repeat([i], 10) for i in 1:7]...)
+
+# pick out a particular topic to look at, say the first one:
+class = 1
+docs_in_class = docs[classes .== class]
+docs_out_of_class = setdiff(docs, docs_in_class)
+
+# make a corpus just from that topic:
+c = Corpus(vcat(docs_in_class...))
+
+# get 20 equispaced points
+endpoints = intervals(c)
+
+# measure the vocabulary size V in increasing steps up to N(c)
+v = [V(c[1:t]) for t in endpoints]
+
+# measure the expected vocabulary size E(V) over the same intervals,
+# conditioned on the whole within-topic corpus but interpolated to timepoint t
+# with binomial expected value (Baayen equation 2.42)
+ev = [V(BinomialExpectation(), c; t = t) for t in endpoints]
+
+fig = Figure()
+ax = Axis(fig[1, 1])
+lines!(ax, endpoints, v; color = :black, linewidth = 5)
+lines!(ax, endpoints, ev; color = :black, linewidth = 5, linestyle = :dot)
+
+# now do an experiment where we repeatedly substitute the last in-topic document with
+# one of the out-of-class docs and see how that changes the tail-end of the observed
+# vocabulary size as well as the whole curve of expected values
+for doc in docs_out_of_class
+	temp_docs = docs_in_class[1:(end - 1)]
+	push!(temp_docs, doc)
+	c′ = Corpus(vcat(temp_docs...))
+	local endpoints = intervals(c′)
+	local v = [V(c′[1:t]) for t in endpoints]
+	local ev = [V(BinomialExpectation(), c′; t = t) for t in endpoints]
+	lines!(ax, endpoints, v; color = RGBA(1, 0.5, 0, 0.1))
+	lines!(ax, endpoints, ev; color = RGBA(1, 0.5, 0, 0.1), linestyle = :dot)
+end
+
+# you could also look at the curve of the growth rate P (Baayen section 2.5):
+growth_rate = [P(BinomialExpectation(), c; t = t) for t in endpoints]
+
+# or the coefficient of loss (Baayen 2.27):
+l = CL(c)
+
+# we might expect both of these values to be higher when mixing in out-of-class douments
 
 
